00709d173fc8582084ce862d358965987734434d
mysil96 <82621911+mysil96@users.noreply.github.com>
2023-07-18 20:44:54 +0200

    update 483 files and delete 3 files

--- Copy.bat
+++ Copy.bat
@@ -1,1 +1,1 @@
-copy "C:/OFFLINE/GitHub/flipperzero-mysil/*.tgz"  "C:/Users/Thomas/Google Drive Streaming/My Drive/Flipper/FW-Release/Mysil-X/"
+copy "*.tgz"  "C:/Users/Thomas/Google Drive Streaming/My Drive/Flipper/FW-Release/Mysil-X/"
