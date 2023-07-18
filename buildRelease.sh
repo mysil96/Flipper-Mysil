de91c0cfe695f61ad748665116e35bbfb205aaef
mysil96 <82621911+mysil96@users.noreply.github.com>
2023-07-18 12:58:03 +0200

    update .gitignore and buildRelease.sh

--- buildRelease.sh
+++ buildRelease.sh
@@ -1,16 +1,17 @@
 rm -rf MYSIL-X*-*-*.tgz MYSIL-X*-*-*.zip MYSIL-X* 
 
-#git pull
+git pull
 
-#./fbt.cmd updater_package
+./fbt.cmd updater_package
 
 DATE_VAR=`date +%m%d`
 TIME_VAR=`date +%H%M`
 VER_VAR=`git log -1 --pretty=%B`
 
 mv dist/f7-C/f7-update-MYSIL-X "MYSIL-X-$DATE_VAR-$TIME_VAR-$VER_VAR"
-zip -rq "MYSIL-X-$DATE_VAR-$TIME_VAR-$VER_VAR.zip" "MYSIL-X-$DATE_VAR-$TIME_VAR-$VER_VAR"
-tar -czf "MYSIL-X-$DATE_VAR-$TIME_VAR-$VER_VAR.tgz" "MYSIL-X-$DATE_VAR-$TIME_VAR-$VER_VAR"
+#zip -rq "MYSIL-X-$DATE_VAR-$TIME_VAR.zip" "MYSIL-X-$DATE_VAR-$TIME_VAR-$VER_VAR"
+tar -czf "MYSIL-X-$DATE_VAR-$TIME_VAR.tgz" "MYSIL-X-$DATE_VAR-$TIME_VAR-$VER_VAR"
+
 #./Copy.bat
-read -p "Press any key to resume ..."
+
 msg "*" BUILD COMPLETED, TGZ BACKED UP TO GDRIVE" "MYSIL-X-$DATE_VAR-$TIME_VAR-$VER_VAR
