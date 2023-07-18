57d37647e0b4721e40838c46275e981586041883
mysil96 <82621911+mysil96@users.noreply.github.com>
2023-07-18 13:05:36 +0200

    update buildRelease.sh

--- buildRelease.sh
+++ buildRelease.sh
@@ -9,7 +9,7 @@ TIME_VAR=`date +%H%M`
 VER_VAR=`git log -1 --pretty=%B`
 
 mv dist/f7-C/f7-update-MYSIL-X "MYSIL-X-$DATE_VAR-$TIME_VAR-$VER_VAR"
-#zip -rq "MYSIL-X-$DATE_VAR-$TIME_VAR.zip" "MYSIL-X-$DATE_VAR-$TIME_VAR-$VER_VAR"
+zip -rq "MYSIL-X-$DATE_VAR-$TIME_VAR.zip" "MYSIL-X-$DATE_VAR-$TIME_VAR-$VER_VAR"
 tar -czf "MYSIL-X-$DATE_VAR-$TIME_VAR.tgz" "MYSIL-X-$DATE_VAR-$TIME_VAR-$VER_VAR"
 
 #./Copy.bat
