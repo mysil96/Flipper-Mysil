473d63cc7f50aaa861eb68352a09bd4e9daa3243
mysil96 <82621911+mysil96@users.noreply.github.com>
2023-07-18 12:53:14 +0200

    update buildRelease.sh

--- buildRelease.sh
+++ buildRelease.sh
@@ -1,12 +1,16 @@
-rm -rf MYSIL-X*-*-*.tgz MYSIL-X*-*-*.zip MYSIL-X* assets/resources/apps
-git pull
-./fbt.cmd updater_package
+rm -rf MYSIL-X*-*-*.tgz MYSIL-X*-*-*.zip MYSIL-X* 
+
+#git pull
+
+#./fbt.cmd updater_package
+
 DATE_VAR=`date +%m%d`
 TIME_VAR=`date +%H%M`
-
 VER_VAR=`git log -1 --pretty=%B`
-mv dist/f7-C/f7-update-MYSIL-X "MYSIL-X"
-#zip -rq "MYSIL-X-$DATE_VAR-$TIME_VAR-$VER_VAR.zip" "MYSIL-X-$DATE_VAR-$TIME_VAR"
-tar -czf "MYSIL-X-$DATE_VAR-$TIME_VAR-$VER_VAR.tgz" "MYSIL-X"
+
+mv dist/f7-C/f7-update-MYSIL-X "MYSIL-X-$DATE_VAR-$TIME_VAR-$VER_VAR"
+zip -rq "MYSIL-X-$DATE_VAR-$TIME_VAR-$VER_VAR.zip" "MYSIL-X-$DATE_VAR-$TIME_VAR-$VER_VAR"
+tar -czf "MYSIL-X-$DATE_VAR-$TIME_VAR-$VER_VAR.tgz" "MYSIL-X-$DATE_VAR-$TIME_VAR-$VER_VAR"
 #./Copy.bat
+read -p "Press any key to resume ..."
 msg "*" BUILD COMPLETED, TGZ BACKED UP TO GDRIVE" "MYSIL-X-$DATE_VAR-$TIME_VAR-$VER_VAR
